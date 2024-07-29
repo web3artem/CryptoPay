466551beac154a46db0023c308131f8f4b8b3303
cringecode <tyoma.dar@gmail.com>
2024-07-29 22:59:29 +0300

    Relocated DeclarativeBase

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
 from config import settings
 
@@ -10,8 +11,10 @@ engine = create_async_engine(settings.DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
+class Base(DeclarativeBase):
+    pass
+
+
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
-
-
