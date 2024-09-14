dd23069a6e177dc8c367359eb2aa234559a5ef64
cringecode <tyoma.dar@gmail.com>
2024-09-14 19:13:19 +0300

    Added SYNC & ASYNC connections to DB

--- src/database.py
+++ src/database.py
@@ -1,14 +1,18 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator
 
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 
 from config import settings
 
-DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = settings.ASYNC_DATABASE_URL
 
-engine = create_async_engine(settings.DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+async_engine = create_async_engine(settings.ASYNC_DATABASE_URL)
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
+
+sync_engine = create_engine(settings.SYNC_DATABASE_URL)
+sync_session_maker = sessionmaker(bind=sync_engine)
 
 
 class Base(DeclarativeBase):
@@ -18,3 +22,11 @@ class Base(DeclarativeBase):
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+async def get_sync_session() -> Generator[Session, None, None]:
+    session = sync_session_maker()
+    try:
+        yield session
+    finally:
+        session.close()
